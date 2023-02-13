5d4a08a48b394587b898caaad8303f530c19c878
James Weigle <jweigle@emory.edu>
2023-02-13 14:47:02 -0500

    added make_page.py

---
+++ make_page.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Goal: make a page _pages/success.md
+with the following content:
+
+---
+layout: page
+title: Success
+---
+
+Success!
+"""
+
+with open("./_pages/success.md", "w") as file:
+    file.write("""---
+layout: page
+title: Success
+---
+
+Success!
+                """)
